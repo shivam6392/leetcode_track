b83ef2c636a38bd3db4f5470b049be53c6aca7b8
Shivam Dwivedi <119709569+shivam6392@users.noreply.github.com>
2026-09-18 11:25:32 +0530

    feat: add solution for Word Pattern.java

--- LeetCode Solutions/Word Pattern.java
+++ LeetCode Solutions/Word Pattern.java
@@ -4,6 +4,7 @@ class Solution {
         HashMap<String, Character> sh = new HashMap<>();
         HashMap<Character, String> rev = new HashMap<>();
         if(pattern.length() != words.length) return false;
+        
         for(int i = 0; i < words.length; i++) {
             char ch = pattern.charAt(i);
             if(sh.containsKey(words[i])) {
